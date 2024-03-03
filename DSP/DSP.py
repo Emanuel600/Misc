2c56496f3d2cd4159d246b82d9145a70fb75ca6b
Emanuel600 <78040463+Emanuel600@users.noreply.github.com>
2024-03-02 22:02:26 -0300

    DSP Added, already has some stuff (~3 hours of work)

---
+++ DSP/DSP.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Signal:
+    # Basic Methods
+    def __init__(self, lower_bound, upper_bound, title=None):
+        self.n = np.arange(lower_bound, upper_bound + 1, 1)
+        self.val = np.zeros(self.n.shape)
+        if (title != None):
+            self.title = title
+        else:
+            self.title = "Signal"
+
+    def __imul__(self, s):
+        self.val = np.multiply(self.val, s.val)
+        return self
+
+    def __iadd__(self, s):
+        self.val += s.val
+        return self
+
+    def __isub__(self, s):
+        self.val -= s.val
+        return self
+
+    # Class Methods
+    # Impulse signal
+    def impulse(self, n, A=1):
+        temp = np.where(self.n == n)
+        if (not temp[0].size):
+            print("Invalid 'n'")
+            return
+        self.val[temp] += A
+        return
+
+    # Exponential Signal
+    def exp(self, k, A=1, off=0):
+        self.val = A*np.exp(k*(self.n - off))
+        return
+
+    # Sine Signal
+    def sin(self, w, A=1):
+        self.val = A*np.sin(w*self.n)
+        return
+
+    # Cosine Signal
+    def cos(self, w, A=1):
+        self.val = A*np.cos(w*self.n)
+        return
+
+    def add_noise(self, var, mean=0.1):
+        self.val += np.random.normal(mean, var/100, self.val.shape)
+
+    def get_sig(self):
+        return self.val
+
+    def set_sig(self, new_val):
+        try:
+            self.val = new_val
+        except:
+            print("Error setting signal")
+
+    # Plot Signal
+    def plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        # Titles and labels
+        ax.set_title(self.title)
+        ax.set_xlabel("n")
+        ax.set_ylabel("y[n]")
+        # Plot
+        plt.stem(self.n, self.val)
+
+# Plot de Conveniência
+
+
+def plot(signal, title="Signal"):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    # Titles and labels
+    ax.set_title(title)
+    ax.set_xlabel("n")
+    ax.set_ylabel("y[n]")
+    # Plot
+    plt.stem(signal[1], signal[0])
+
+
+def cos(start, end, w=1):
+    n = np.arange(start, end+1, 1)
+    return np.cos(w*n), n
+
+
+def sin(start, end, w=1):
+    n = np.arange(start, end+1, 1)
+    return np.sin(w*n), n
+
+
+# fDSP do professor
+def impseq(n0, n1, n2):
+
+    #      Generates x(n) = delta(n-n0); n1 <= n <= n2
+    #      ----------------------------------------------
+    #      [x,n] = impseq(n0,n1,n2)
+    #
+
+    n = np.array(range(n1, n2+1))
+    # Shift by n0
+    x = n-n0
+    for i in range(len(x)):
+        # Only x[n0] == 0
+        if x[i] != 0:
+            x[i] = 0
+        else:
+            x[i] = 1
+    # x = (n-n0) == 0
+    return [x, n]
+
+
+def stepseq(n0, n1, n2):
+    #     Generates x(n) = u(n-n0); n1 <= n <= n2
+    #     ------------------------------------------
+    #     [x,n] = stepseq(n0,n1,n2)
+    #
+    n = np.arange(n1, n2+1)
+    x = n-n0
+    for i in range(len(x)):
+        if x[i] < 0:
+            x[i] = 0
+        else:
+            x[i] = 1
+    # x = [(n-n0) >= 0]
+    return [x, n]
+
+
+def sigadd(x1, n1, x2, n2):
+    # % implements y(n) = x1(n)+x2(n)
+    # % -----------------------------
+    # % [y,n] = sigadd(x1,n1,x2,n2)
+    # % y = sum sequence over n, which includes n1 and n2
+    # % x1 = first sequence over n1
+    # % x2 = second sequence over n2 (n2 can be different from n1)
+    # %
+    n = np.arange(min(n1.min(0), n2.min(0)), max(
+        n1.max(0), n2.max(0))+1)  # duration of y(n)
+    y1 = np.zeros((1, len(n)))  # initialization
+    y1 = y1[0, :]
+    y2 = y1.copy()
+    y1[np.nonzero(np.logical_and((n >= n1.min(0)),
+                  (n <= n1.max(0))) == 1)] = x1.copy()
+    y2[np.nonzero(np.logical_and((n >= n2.min(0)),
+                  (n <= n2.max(0))) == 1)] = x2.copy()
+    y = y1+y2
+    return [y, n]
+
+
+def sigmult(x1, n1, x2, n2):
+    # % implements y(n) = x1(n)*x2(n)
+    # % -----------------------------
+    # % [y,n] = sigmult(x1,n1,x2,n2)
+    # % y = product sequence over n, which includes n1 and n2
+    # % x1 = first sequence over n1
+    # %
+    n = np.arange(min(n1.min(0), n2.min(0)), max(
+        n1.max(0), n2.max(0))+1)  # duration of y(n)
+    y1 = np.zeros((1, len(n)))  # initialization
+    y1 = y1[0, :]
+    y2 = y1.copy()
+    y1[np.nonzero(np.logical_and((n >= n1.min(0)),
+                  (n <= n1.max(0))) == 1)] = x1.copy()
+    y2[np.nonzero(np.logical_and((n >= n2.min(0)),
+                  (n <= n2.max(0))) == 1)] = x2.copy()
+    y = y1*y2
+    return [y, n]
+
+
+def sigshift(x, m, k):
+    # % implements y(n) = x(n-k)
+    # % -------------------------
+    # % [y,n] = sigshift(x,m,k)
+    # %
+    n = m+k
+    y = x.copy()
+    return [y, n]
+
+
+def sigfold(x, n):
+    # % implements y(n) = x(-n)
+    # % -----------------------
+    # % [y,n] = sigfold(x,n)
+    # %
+    y = np.flip(x)
+    n = -np.flip(n)
+    return [y, n]
+
+
+def conv_m(x, nx, h, nh):
+    # Modified convolution routine for signal processing
+    # --------------------------------------------------
+    # [y,ny] = conv_m(x,nx,h,nh)
+    # [y,ny] = convolution result
+    # [x,nx] = first signal
+    # [h,nh] = second signal
+    #
+    nyb = nx[0]+nh[0]
+    nye = nx[len(x)-1] + nh[len(h)-1]
+    ny = np.arange(nyb, nye+1)
+    y = np.convolve(x, h)
+    return [y, ny]
+
+# Adicionais por mim
---
+++ DSP/Ex.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 09:54:19 2024
+
+@author: Aluno
+"""
+# %% Imports / Setup
+import DSP
+
+import numpy as np
+# %% 2.1.1
+s = DSP.Signal(-5, 15)
+s.impulse(-2, 3)
+s.impulse(0, 2)
+s.impulse(3, -1)
+s.impulse(7, 5)
+s.plot()
+# %% 2.1.2
+s = DSP.Signal(-10, 10, "Ex 2.1.2")
+s.exp(0.5)
+s1 = DSP.Signal(-10, 10, "Partial")
+for n in range(-10, 11, 2):
+    s1.impulse(n, 1)
+s *= s1
+s.plot()
+
+# %% 2.1.3
+sig = DSP.stepseq(0, -1, 20)
+sig[0] *= 10
+sig[0] += -5*DSP.stepseq(5, -1, 20)[0] - 10*DSP.stepseq(10, -1, 20)[0] \
+    + 5*DSP.stepseq(15, -1, 20)[0]
+DSP.plot(sig, "Ex 2.1.3")
+
+# %% 2.1.4
+sig = DSP.stepseq(-20, -25, 15)
+sig[0] -= DSP.stepseq(10, -25, 15)[0]
+DSP.plot(sig, "Degraus para Ex 2.1.4")
+sig[0] = np.exp(0.1*sig[1]) + sig[0]
+DSP.plot(sig, "Ex 2.1.4")
+# %% 2.1.5
+sig = DSP.cos(-200, 200, 0.49*np.pi)
+# 'sig' becomes a tuple for some reason
+sig = np.array(sig)
+sig[0] += 5*DSP.cos(-200, 200, 0.51*np.pi)[0]
+DSP.plot(sig, "cos")
+
+# %% 2.1.6
+s = DSP.Signal(-200, 200)
+s1 = DSP.Signal(-200, 200)
+s.sin(0.01*np.pi, 2)
+s.plot()
+s1.cos(0.5*np.pi)
+s1.plot()
+s *= s1
+s.plot()
+
+# %% 2.1.7
+s = DSP.Signal(-200, 200)
+s.add_noise(1/10, 0)
+s.plot()
+# %%
